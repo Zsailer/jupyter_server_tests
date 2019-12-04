cc372a69a955b6455e866df9abe68523b939cd11
Zsailer <zachsailer@gmail.com>
2019-12-04 11:59:44 -0600

    handle singleton issues in gateway

--- tests/test_gateway.py
+++ tests/test_gateway.py
@@ -129,9 +129,13 @@ async def mock_gateway_request(url, **kwargs):
 
 
 mocked_gateway = patch('jupyter_server.gateway.managers.gateway_request', mock_gateway_request)
-mock_gateway_url = 'http://mock-gateway-server:8889'
+#mock_gateway_url = 'http://mock-gateway-server:8889'
 mock_http_user = 'alice'
 
+
+@pytest.fixture
+def mock_gateway_url(http_port):
+    return 'http://mock-gateway-server:{}'.format(http_port)
 
     #
     # @classmethod
@@ -159,28 +163,43 @@ mock_http_user = 'alice'
 
 
 @pytest.fixture
-def gateway_env(monkeypatch):
+def init_gateway(monkeypatch, mock_gateway_url):
+    GatewayClient.clear_instance()
     monkeypatch.setenv('JUPYTER_GATEWAY_URL', mock_gateway_url)
     monkeypatch.setenv('JUPYTER_GATEWAY_HTTP_USER', mock_http_user)
     monkeypatch.setenv('JUPYTER_GATEWAY_REQUEST_TIMEOUT', '44.4')
     monkeypatch.setenv('JUPYTER_GATEWAY_CONNECT_TIMEOUT', '44.4')
+    yield 
+    GatewayClient.clear_instance()
 
 
-async def test_gateway_cli_options():
-    app = ServerApp()
-    app.initialize(["--gateway-url='" + mock_gateway_url + "'",
-                    "--GatewayClient.http_user='" + mock_http_user + "'",
-                    '--GatewayClient.connect_timeout=44.4',
-                    '--GatewayClient.request_timeout=44.4'])
-
-    assert app.gateway_config.gateway_enabled is True
-    assert app.gateway_config.url == mock_gateway_url
-    assert app.gateway_config.http_user == mock_http_user
-    assert app.gateway_config.connect_timeout == app.gateway_config.request_timeout
-    assert app.gateway_config.connect_timeout == 44.4
+@pytest.fixture
+def config(mock_gateway_url):
+    return Config({
+        'GatewayClient': {
+            'url': mock_gateway_url,
+            'http_user': mock_http_user,
+            'connect_timeout': 44.4,
+            'request_timeout': 44.4
+        } 
+    })
 
 
-async def test_gateway_env_options(gateway_env, serverapp):
+# async def test_gateway_cli_options():
+#     app = ServerApp()
+#     app.initialize(["--gateway-url='" + mock_gateway_url + "'",
+#                     "--GatewayClient.http_user='" + mock_http_user + "'",
+#                     '--GatewayClient.connect_timeout=44.4',
+#                     '--GatewayClient.request_timeout=44.4'])
+
+#     assert app.gateway_config.gateway_enabled is True
+#     assert app.gateway_config.url == mock_gateway_url
+#     assert app.gateway_config.http_user == mock_http_user
+#     assert app.gateway_config.connect_timeout == app.gateway_config.request_timeout
+#     assert app.gateway_config.connect_timeout == 44.4
+
+
+async def test_gateway_env_options(init_gateway, serverapp):
     assert serverapp.gateway_config.gateway_enabled is True
     assert serverapp.gateway_config.url == mock_gateway_url
     assert serverapp.gateway_config.http_user == mock_http_user
@@ -188,7 +207,7 @@ async def test_gateway_env_options(gateway_env, serverapp):
     assert serverapp.gateway_config.connect_timeout == 44.4
 
 
-async def test_gateway_class_mappings(gateway_env, serverapp):
+async def test_gateway_class_mappings(init_gateway, serverapp):
     # Ensure appropriate class mappings are in place.
     assert serverapp.kernel_manager_class.__name__ == 'GatewayKernelManager'
     assert serverapp.session_manager_class.__name__ == 'GatewaySessionManager'
@@ -417,3 +436,218 @@ async def delete_kernel(fetch, kernel_id):
         )
         assert r.code == 204
         assert r.reason == 'No Content'
+
+
+
+"""Test GatewayClient"""
+import json
+import os
+import pytest
+import tornado
+import uuid
+from datetime import datetime
+from tornado import gen
+from tornado.httpclient import HTTPRequest, HTTPResponse, HTTPError
+from ipython_genutils.py3compat import str_to_unicode
+from jupyter_server.serverapp import ServerApp
+
+from traitlets.config import Config
+
+from jupyter_server.gateway.managers import GatewayClient
+
+from unittest.mock import patch
+from io import StringIO
+from .conftest import expected_http_error
+
+# Run all tests in this module using asyncio's event loop
+pytestmark = pytest.mark.asyncio
+
+
+def generate_kernelspec(name):
+    argv_stanza = ['python', '-m', 'ipykernel_launcher', '-f', '{connection_file}']
+    spec_stanza = {'spec': {'argv': argv_stanza, 'env': {}, 'display_name': name, 'language': 'python', 'interrupt_mode': 'signal', 'metadata': {}}}
+    kernelspec_stanza = {'name': name, 'spec': spec_stanza, 'resources': {}}
+    return kernelspec_stanza
+
+
+# We'll mock up two kernelspecs - kspec_foo and kspec_bar
+kernelspecs = {'default': 'kspec_foo', 'kernelspecs': {'kspec_foo': generate_kernelspec('kspec_foo'), 'kspec_bar': generate_kernelspec('kspec_bar')}}
+
+
+# maintain a dictionary of expected running kernels.  Key = kernel_id, Value = model.
+running_kernels = dict()
+
+
+def generate_model(name):
+    """Generate a mocked kernel model.  Caller is responsible for adding model to running_kernels dictionary."""
+    dt = datetime.utcnow().isoformat() + 'Z'
+    kernel_id = str(uuid.uuid4())
+    model = {'id': kernel_id, 'name': name, 'last_activity': str(dt), 'execution_state': 'idle', 'connections': 1}
+    return model
+
+
+async def mock_gateway_request(url, **kwargs):
+    method = 'GET'
+    if kwargs['method']:
+        method = kwargs['method']
+
+    request = HTTPRequest(url=url, **kwargs)
+
+    endpoint = str(url)
+
+    # Fetch all kernelspecs
+    if endpoint.endswith('/api/kernelspecs') and method == 'GET':
+        response_buf = StringIO(str_to_unicode(json.dumps(kernelspecs)))
+        response = await gen.maybe_future(HTTPResponse(request, 200, buffer=response_buf))
+        return response
+
+    # Fetch named kernelspec
+    if endpoint.rfind('/api/kernelspecs/') >= 0 and method == 'GET':
+        requested_kernelspec = endpoint.rpartition('/')[2]
+        kspecs = kernelspecs.get('kernelspecs')
+        if requested_kernelspec in kspecs:
+            response_buf = StringIO(str_to_unicode(json.dumps(kspecs.get(requested_kernelspec))))
+            response = await gen.maybe_future(HTTPResponse(request, 200, buffer=response_buf))
+            return response
+        else:
+            raise HTTPError(404, message='Kernelspec does not exist: %s' % requested_kernelspec)
+
+    # Create kernel
+    if endpoint.endswith('/api/kernels') and method == 'POST':
+        json_body = json.loads(kwargs['body'])
+        name = json_body.get('name')
+        env = json_body.get('env')
+        kspec_name = env.get('KERNEL_KSPEC_NAME')
+        assert name == kspec_name   # Ensure that KERNEL_ env values get propagated
+        model = generate_model(name)
+        running_kernels[model.get('id')] = model  # Register model as a running kernel
+        response_buf = StringIO(str_to_unicode(json.dumps(model)))
+        response = await gen.maybe_future(HTTPResponse(request, 201, buffer=response_buf))
+        return response
+
+    # Fetch list of running kernels
+    if endpoint.endswith('/api/kernels') and method == 'GET':
+        kernels = []
+        for kernel_id in running_kernels.keys():
+            model = running_kernels.get(kernel_id)
+            kernels.append(model)
+        response_buf = StringIO(str_to_unicode(json.dumps(kernels)))
+        response = await gen.maybe_future(HTTPResponse(request, 200, buffer=response_buf))
+        return response
+
+    # Interrupt or restart existing kernel
+    if endpoint.rfind('/api/kernels/') >= 0 and method == 'POST':
+        requested_kernel_id, sep, action = endpoint.rpartition('/api/kernels/')[2].rpartition('/')
+
+        if action == 'interrupt':
+            if requested_kernel_id in running_kernels:
+                response = await gen.maybe_future(HTTPResponse(request, 204))
+                return response
+            else:
+                raise HTTPError(404, message='Kernel does not exist: %s' % requested_kernel_id)
+        elif action == 'restart':
+            if requested_kernel_id in running_kernels:
+                response_buf = StringIO(str_to_unicode(json.dumps(running_kernels.get(requested_kernel_id))))
+                response = await gen.maybe_future(HTTPResponse(request, 204, buffer=response_buf))
+                return response
+            else:
+                raise HTTPError(404, message='Kernel does not exist: %s' % requested_kernel_id)
+        else:
+            raise HTTPError(404, message='Bad action detected: %s' % action)
+
+    # Shutdown existing kernel
+    if endpoint.rfind('/api/kernels/') >= 0 and method == 'DELETE':
+        requested_kernel_id = endpoint.rpartition('/')[2]
+        running_kernels.pop(requested_kernel_id)  # Simulate shutdown by removing kernel from running set
+        response = await gen.maybe_future(HTTPResponse(request, 204))
+        return response
+
+    # Fetch existing kernel
+    if endpoint.rfind('/api/kernels/') >= 0 and method == 'GET':
+        requested_kernel_id = endpoint.rpartition('/')[2]
+        if requested_kernel_id in running_kernels:
+            response_buf = StringIO(str_to_unicode(json.dumps(running_kernels.get(requested_kernel_id))))
+            response = await gen.maybe_future(HTTPResponse(request, 200, buffer=response_buf))
+            return response
+        else:
+            raise HTTPError(404, message='Kernel does not exist: %s' % requested_kernel_id)
+
+
+mocked_gateway = patch('jupyter_server.gateway.managers.gateway_request', mock_gateway_request)
+#mock_gateway_url = 'http://mock-gateway-server:8889'
+mock_http_user = 'alice'
+
+
+@pytest.fixture
+def mock_gateway_url(http_port):
+    return 'http://mock-gateway-server:{}'.format(http_port)
+
+    #
+    # @classmethod
+    # def setup_class(cls):
+    #     GatewayClient.clear_instance()
+    #     super(TestGateway, cls).setup_class()
+    #
+    # @classmethod
+    # def teardown_class(cls):
+    #     GatewayClient.clear_instance()
+    #     super(TestGateway, cls).teardown_class()
+    #
+    # @classmethod
+    # def get_patch_env(cls):
+    #     test_env = super(TestGateway, cls).get_patch_env()
+    #     test_env.update({'JUPYTER_GATEWAY_URL': TestGateway.mock_gateway_url,
+    #                      'JUPYTER_GATEWAY_REQUEST_TIMEOUT': '44.4'})
+    #     return test_env
+    #
+    # @classmethod
+    # def get_argv(cls):
+    #     argv = super(TestGateway, cls).get_argv()
+    #     argv.extend(['--GatewayClient.connect_timeout=44.4', '--GatewayClient.http_user=' + TestGateway.mock_http_user])
+    #     return argv
+
+
+@pytest.fixture
+def init_gateway(monkeypatch, mock_gateway_url):
+    GatewayClient.clear_instance()
+    monkeypatch.setenv('JUPYTER_GATEWAY_URL', mock_gateway_url)
+    monkeypatch.setenv('JUPYTER_GATEWAY_HTTP_USER', mock_http_user)
+    monkeypatch.setenv('JUPYTER_GATEWAY_REQUEST_TIMEOUT', '44.4')
+    monkeypatch.setenv('JUPYTER_GATEWAY_CONNECT_TIMEOUT', '44.4')
+    yield 
+    GatewayClient.clear_instance()
+
+
+@pytest.fixture
+def config(mock_gateway_url):
+    return Config({
+        'GatewayClient': {
+            'url': mock_gateway_url,
+            'http_user': mock_http_user,
+            'connect_timeout': 44.4,
+            'request_timeout': 44.4
+        } 
+    })
+
+
+# async def test_gateway_cli_options():
+#     app = ServerApp()
+#     app.initialize(["--gateway-url='" + mock_gateway_url + "'",
+#                     "--GatewayClient.http_user='" + mock_http_user + "'",
+#                     '--GatewayClient.connect_timeout=44.4',
+#                     '--GatewayClient.request_timeout=44.4'])
+
+#     assert app.gateway_config.gateway_enabled is True
+#     assert app.gateway_config.url == mock_gateway_url
+#     assert app.gateway_config.http_user == mock_http_user
+#     assert app.gateway_config.connect_timeout == app.gateway_config.request_timeout
+#     assert app.gateway_config.connect_timeout == 44.4
+
+
+async def test_gateway_env_options(init_gateway, serverapp, mock_gateway_url):
+    assert serverapp.gateway_config.gateway_enabled is True
+    assert serverapp.gateway_config.url == mock_gateway_url
+    assert serverapp.gateway_config.http_user == mock_http_user
+    assert serverapp.gateway_config.connect_timeout == serverapp.gateway_config.request_timeout
+    assert serverapp.gateway_config.connect_timeout == 44.4
+
